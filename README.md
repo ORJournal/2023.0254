9f06af677ca19e30c2ecda8a42f21586378c7439
Pedro Zattoni Scroccaro <pedroszattoni@users.noreply.github.com>
2024-03-13 17:12:31 +0100

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,66 @@
-# 2023.0254
+
+[![Operations Research Journal Logo](https://orjournal.github.io/OperationsReseachHeader.jpg)](https://pubsonline.informs.org/journal/opre)
+
+# Learning in Inverse Optimization: Incenter Cost, Augmented Suboptimality Loss, and Algorithms
+
+This archive is distributed in association with the journal [Operations Research](https://pubsonline.informs.org/journal/opre) under the [MIT License](LICENSE).
+
+The software and data in this repository are a snapshot of the software and data that were used in the research reported in the paper [Learning in Inverse Optimization: Incenter Cost, Augmented Suboptimality Loss, and Algorithms](https://doi.org/10.1287/opre.2023.0254) by Pedro Zattoni Scroccaro, Bilge Atasoy, and Peyman Mohajerin Esfahani. The snapshot is based on [this SHA](https://github.com/tkralphs/ORJournalTemplate/commit/7588d8908e79fd1b30a933d3553ebf18bb906bb9) in the development repository. 
+
+## Cite
+
+To cite the contents of this repository, please cite both the paper and this repo, using their respective DOIs.
+
+https://doi.org/10.1287/opre.2023.0254
+
+https://doi.org/10.1287/opre.2023.0254.cd
+
+Below is the BibTex for citing this snapshot of the repository.
+
+```
+@article{zattoniscroccaro2023learning,
+  author =        {Zattoni Scroccaro, Pedro and Atasoy, Bilge and Mohajerin Esfahani, Peyman},
+  publisher =     {Operations Research},
+  title =         {{Learning in Inverse Optimization: Incenter Cost, Augmented Suboptimality Loss, and Algorithms}},
+  year =          {2024},
+  doi =           {10.1287/opre.2023.0254.cd},
+  url =           {https://github.com/ORJournal/2023.0254},
+}  
+```
+
+## Description
+
+The goal of this software is to replicate the numerical experiments in the paper "Learning in Inverse Optimization: Incenter Cost, Augmented Suboptimality Loss, and Algorithms" by Pedro Zattoni Scroccaro, Bilge Atasoy, and Peyman Mohajerin Esfahani.
+
+## Dependencies
+
+The code in this repository requires the following dependencies. The dependency version number corresponds to the version of the package with which the code was tested.
+
+- [Python](https://www.python.org/) - 3.10.11
+- [InvOpt](https://github.com/pedroszattoni/InvOpt) - 0.0.7
+- [NumPy](https://numpy.org/) - 1.24.3
+- [Gurobi / gurobipy](https://www.gurobi.com/) - 10.0.1
+- [matplotlib](https://matplotlib.org/) - 3.7.1
+- [polytope](https://tulip-control.github.io/polytope/) - 0.2.3
+- [CVXPY](https://www.cvxpy.org/) - 1.3.1
+
+## Replicating results
+
+The scripts are located [here](https://github.com/ORJournal/2023.0254/tree/master/scripts) and the data are located [here](https://github.com/ORJournal/2023.0254/tree/master/data).
+
+- To replicate the results in Section 6.1, as well as their respective results in Appendix D.2, run the script `experiments_6_1_consistent_data.py`.
+- To replicate the results in Section 6.2, as well as their respective results in Appendix D.2, run the script `experiments_6_2_inconsistent_data.py`.
+- To replicate the results in Section 6.3, as well as their respective results in Appendix D.2, run the script `experiments_6_3_mixed_integer.py`.
+- To replicate the results of Figure 6a  in Section 6.4, as well as the results in Appendix D.3, run the script `experiments_6_4_SAMD.py`. **Note 1**: before running the script, the variable `path_to_data` needs to be edited so that it is equal to the complete path to the location of the data `dataset_FOM.p`. This data corresponds to precomputed training and test datasets. **Note 2**: the exact results of this experiment depend on the runtime of each iteration of the algorithm, thus, they will change for different hardware specifications. The results shown in the paper were computed using 1 thread of an Intel Xeon Gold 5218 CPU with a 2.30GHz clock speed and 32GB of RAM.
+- To replicate the results of Figure 6b  in Section 6.4, run the script `experiments_6_4_SAMD_batches.py`. **Note 1**: before running the script, the variable `path_to_data` needs to be edited so that it is equal to the complete path to the location of the data `dataset_FOM.p`. This data corresponds to precomputed training and test datasets. **Note 2**: the exact results of this experiment depend on the runtime of each iteration of the algorithm, thus, they will change for different hardware specifications. The results shown in the paper were computed using 1 thread of an Intel Xeon Gold 5218 CPU with a 2.30GHz clock speed and 32GB of RAM.
+- To replicate the results of Appendix D.1, run the script `experiments_AD_1_BCWP.py`. **Note**: before running the script, the variable `path_to_data` needs to be edited so that it is equal to the complete path to the location of the data `wpbc_data.csv`. This data corresponds the [Breast Cancer Wisconsin Prognostic dataset](https://archive.ics.uci.edu/dataset/16/breast+cancer+wisconsin+prognostic).
+
+**Warning**: it may take hours to replicate some of the results in the paper. Tables 1, 2, and 3 in the paper show an estimate of the time required to replicate the results of Sections 6.1, 6.2, and 6.3. To quickly test the scripts in simpler examples, one can change the simulation parameters in the scripts to decrease the size of the datasets, the dimensions of the problem, the resolution of the plots, and/or the number of runs (i.e., the number of times the experiment is repeated).  
+
+## Ongoing Development
+
+These examples are part of the examples in the [InvOpt](https://github.com/pedroszattoni/InvOpt) package.
+
+## Support
+
+For support in using this software, submit an [issue](https://github.com/ORJournal/2023.0254/issues/new).
---
+++ scripts/experiments_AD_1_BCWP.py
@@ -0,0 +1,255 @@
+"""
+Script for generating the results of Appendix D.1.
+
+Dataset: Breast Cancer Wisconsin Prognostic (BCWP)
+https://archive.ics.uci.edu/dataset/16/breast+cancer+wisconsin+prognostic
+
+Author: Pedro Zattoni Scroccaro
+"""
+
+
+import time
+import numpy as np
+import gurobipy as gp
+import invopt as iop
+from sklearn import svm
+from sklearn import kernel_ridge
+from utils import mean_percentiles, L1
+
+np.random.seed(0)
+
+
+def FOP_MIQP(theta, s):
+    """Forward optimization problem."""
+    _, _, _, w = s
+    u = len(w)
+    cQ = u + 1 + u + 1
+    Qyy = theta[0]
+    Q = theta[1:(1 + cQ)]
+    q = theta[(1 + cQ):]
+
+    if len(theta) != (1 + cQ + cQ):
+        raise Exception('Dimentions do not match!')
+
+    mdl = gp.Model('MIQP')
+    mdl.setParam('OutputFlag', 0)
+    y = mdl.addVar(vtype=gp.GRB.CONTINUOUS, name='y')
+    z = mdl.addVar(vtype=gp.GRB.BINARY, name='z')
+
+    phi1_wz = [wi for wi in w] + [z] + [z*wi for wi in w] + [1]
+    phi2_wz = [wi for wi in w] + [z] + [z*wi for wi in w] + [1]
+
+    mdl.setObjective(
+        Qyy*y**2 + y*gp.quicksum(Q[i]*phi1_wz[i] for i in range(cQ))
+        + gp.quicksum(q[i]*phi2_wz[i] for i in range(cQ)), gp.GRB.MINIMIZE
+    )
+
+    mdl.optimize()
+
+    y_opt = np.array([y.X])
+    z_opt = np.array([round(z.X)])
+
+    return y_opt, z_opt
+
+
+def load_data(train_test_slip, path_to_data):
+    """Load and preprosses BCWP data."""
+    dataset = np.genfromtxt(path_to_data, delimiter=',')
+
+    # Signal-response data
+    S = dataset[:, 2:].copy()
+    X = dataset[:, :2].copy()
+    X[:, [1, 0]] = X[:, [0, 1]]
+
+    N, m = S.shape
+    N_train = round(N*(1-train_test_slip))
+    # N_test = round(N*train_test_slip)
+
+    train_idx = np.random.choice(N, N_train, replace=False)
+    train_mask = np.zeros(N, dtype=bool)
+    train_mask[train_idx] = True
+
+    # Split data into train/test
+    S_train = S[train_mask, :].copy()
+    X_train = X[train_mask, :].copy()
+    S_test = S[~train_mask, :].copy()
+    X_test = X[~train_mask, :].copy()
+
+    return S_train, X_train, S_test, X_test
+
+
+def IO_preprocessing(S_train, X_train, S_test, X_test):
+    """Preprocess data for IO."""
+    A = -np.eye(1)
+    B = np.zeros((1, 1))
+    c = np.zeros((1))
+
+    N_train = len(S_train)
+    N_test = len(S_test)
+
+    # Create dataset for IO
+    dataset_train = []
+    for i in range(N_train):
+        s_hat = (A, B, c, S_train[i, :])
+        x_hat = (np.array([X_train[i, 0]]), np.array([X_train[i, 1]]))
+        dataset_train.append((s_hat, x_hat))
+
+    dataset_test = []
+    for i in range(N_test):
+        s_hat = (A, B, c, S_test[i, :])
+        x_hat = (np.array([X_train[i, 0]]), np.array([X_test[i, 1]]))
+        dataset_test.append((s_hat, x_hat))
+
+    return dataset_train, dataset_test
+
+
+def phi1(w, z):
+    """Feature function."""
+    return np.concatenate((w, z, w*z, [1]))
+
+
+def phi2(w, z):
+    """Feature function."""
+    return np.concatenate((w, z, w*z, [1]))
+
+
+def phi(s, x):
+    """Transform phi1 and phi2 into phi."""
+    _, _, _, w = s
+    y, z = x
+    return np.concatenate((y*phi1(w, z), phi2(w, z)))
+
+
+def dist_y(x1, x2):
+    """Distance function for continous partof decision vector."""
+    y1, z1 = x1
+    y2, z2 = x2
+    dy = L1(y1, y2)
+    return dy
+
+
+def dist_z(x1, x2):
+    """Distance function for discrete part of decision vector."""
+    y1, z1 = x1
+    y2, z2 = x2
+    dz = L1(z1, z2)
+    return dz
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%% Simulation parameters %%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+train_test_slip = 0.1
+runs = 20
+kappa = 10**3
+
+path_to_data = 'path/to/data'
+
+print('')
+print(f'train_test_slip = {train_test_slip}')
+print(f'runs = {runs}')
+print(f'kappa = {kappa}')
+print('')
+
+
+# %%%%%%%%%%%%%%%%%%%%%%%%%%% Solve IO problem %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+y_diff_train_1_hist = np.empty(runs)
+y_diff_test_1_hist = np.empty(runs)
+z_diff_train_1_hist = np.empty(runs)
+z_diff_test_1_hist = np.empty(runs)
+
+y_diff_train_2_hist = np.empty(runs)
+y_diff_test_2_hist = np.empty(runs)
+z_diff_train_2_hist = np.empty(runs)
+z_diff_test_2_hist = np.empty(runs)
+
+y_diff_train_sk_hist = np.empty(runs)
+y_diff_test_sk_hist = np.empty(runs)
+z_diff_train_sk_hist = np.empty(runs)
+z_diff_test_sk_hist = np.empty(runs)
+
+tic = time.time()
+for run in range(runs):
+    np.random.seed(run)  # Make sure the same random slipt is used
+    S_train, X_train, S_test, X_test = load_data(train_test_slip, path_to_data)
+    dataset_train, dataset_test = IO_preprocessing(
+        S_train, X_train, S_test, X_test
+    )
+
+    # ASL-z
+    theta_IO = iop.mixed_integer_quadratic(dataset_train,
+                                           ('binary', 1, None),
+                                           phi1=phi1,
+                                           phi2=phi2,
+                                           dist_func_z=L1,
+                                           reg_param=kappa,
+                                           add_dist_func_y=False)
+
+    y_diff_train = iop.evaluate(theta_IO, dataset_train, FOP_MIQP, dist_y)
+    y_diff_test = iop.evaluate(theta_IO, dataset_test, FOP_MIQP, dist_y)
+    z_diff_train = iop.evaluate(theta_IO, dataset_train, FOP_MIQP, dist_z)
+    z_diff_test = iop.evaluate(theta_IO, dataset_test, FOP_MIQP, dist_z)
+
+    y_diff_train_1_hist[run] = y_diff_train
+    y_diff_test_1_hist[run] = y_diff_test
+    z_diff_train_1_hist[run] = z_diff_train
+    z_diff_test_1_hist[run] = z_diff_test
+
+    # ASL-yz
+    theta_IO = iop.mixed_integer_quadratic(dataset_train,
+                                           ('binary', 1, None),
+                                           phi1=phi1,
+                                           phi2=phi2,
+                                           dist_func_z=L1,
+                                           reg_param=kappa,
+                                           add_dist_func_y=True)
+
+    y_diff_train = iop.evaluate(theta_IO, dataset_train, FOP_MIQP, dist_y)
+    y_diff_test = iop.evaluate(theta_IO, dataset_test, FOP_MIQP, dist_y)
+    z_diff_train = iop.evaluate(theta_IO, dataset_train, FOP_MIQP, dist_z)
+    z_diff_test = iop.evaluate(theta_IO, dataset_test, FOP_MIQP, dist_z)
+
+    y_diff_train_2_hist[run] = y_diff_train
+    y_diff_test_2_hist[run] = y_diff_test
+    z_diff_train_2_hist[run] = z_diff_train
+    z_diff_test_2_hist[run] = z_diff_test
+
+    # Scikit-learn: regression + classification
+    reg = kernel_ridge.KernelRidge()
+    reg.fit(S_train, X_train[:, 0])
+    y_diff_train_sk = np.mean(np.abs(reg.predict(S_train) - X_train[:, 0]))
+    y_diff_test_sk = np.mean(np.abs(reg.predict(S_test) - X_test[:, 0]))
+    y_diff_train_sk_hist[run] = y_diff_train_sk
+    y_diff_test_sk_hist[run] = y_diff_test_sk
+
+    clf = svm.SVC()
+    clf.fit(S_train, X_train[:, 1])
+    z_diff_train_sk = np.mean(np.abs(clf.predict(S_train) - X_train[:, 1]))
+    z_diff_test_sk = np.mean(np.abs(clf.predict(S_test) - X_test[:, 1]))
+    z_diff_train_sk_hist[run] = z_diff_train_sk
+    z_diff_test_sk_hist[run] = z_diff_test_sk
+
+    print(f'{round(100*(run+1)/runs)}%')
+
+toc = time.time()
+print(f"Simulation time = {round(toc-tic,2)} seconds")
+print('')
+
+print('ASL-z')
+print(f'y_diff train error = {mean_percentiles(y_diff_train_1_hist)[0]}')
+print(f'y_diff test error = {mean_percentiles(y_diff_test_1_hist)[0]}')
+print(f'z_diff train error = {mean_percentiles(z_diff_train_1_hist)[0]}')
+print(f'z_diff test error = {mean_percentiles(z_diff_test_1_hist)[0]}')
+print('')
+
+print('ASL-yz')
+print(f'y_diff train error = {mean_percentiles(y_diff_train_2_hist)[0]}')
+print(f'y_diff test error = {mean_percentiles(y_diff_test_2_hist)[0]}')
+print(f'z_diff train error = {mean_percentiles(z_diff_train_2_hist)[0]}')
+print(f'z_diff test error = {mean_percentiles(z_diff_test_2_hist)[0]}')
+print('')
+
+print('Scikit-learn (regression + classification)')
+print(f'SK y_diff train error = {mean_percentiles(y_diff_train_sk_hist)[0]}')
+print(f'SK y_diff test error = {mean_percentiles(y_diff_test_sk_hist)[0]}')
+print(f'SK z_diff train error = {mean_percentiles(z_diff_train_sk_hist)[0]}')
+print(f'SK z_diff test error = {mean_percentiles(z_diff_test_sk_hist)[0]}')
